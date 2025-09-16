bc43b7f47d3bd4eaa7d2e8ad381803c9db8e5487
Goodyness-dev <adewolegoodness22@gmail.com>
2025-09-16 07:18:28 +0100

    minor fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import React, { useRef } from 'react';
-import {Canvas, useFrame} from '@react-three/fiber'
-import {OrbitControls, Sparkles} from "@react-three/drei"
-import Navbar from './sections/Navbar';
-import Hero from './sections/Hero'
-import About from './sections/About';
-import Project from './sections/Project';
-import Contact from './sections/Contact';
-import Footer from './sections/Footer';
-//import Experience from './sections/Experience';
 
 
 
-const App = () => {
-  return (
-    <main className='max-w-7xl mx-auto'>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Project/>
-    
-      <Contact/>
-      <Footer/>
-    </main>
-  );
-}
 
-export default App;
 
 
 createRoot(document.getElementById('root')).render(
